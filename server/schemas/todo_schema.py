913aa5f4c3df775b89e0b6da73696e26cbd05ba0
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-11-29 14:54:35 -0800

    changes todo schema

--- server/schemas/todo_schema.py
+++ server/schemas/todo_schema.py
@@ -7,7 +7,7 @@ class TodoSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Todo
         load_instance = True
-        fields = ['id', 'item', 'status', 'day', 'user_id']
+        fields = ['id', 'item', 'status', 'date', 'user_id']
 
     item = fields.String(required=True, validate=validate.Length(min=2, max=30))
     status = fields.Boolean(required=True)

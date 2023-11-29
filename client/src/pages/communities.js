960dac42d8279fc7ab62f29f4477753e3ae15f8b
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-29 10:05:27 -0700

    test communities fetch

--- client/src/pages/communities.js
+++ client/src/pages/communities.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Communities = () => {
+  const [communities, setCommunities] = useState(null);
+
+  useEffect(() => {
+    fetch("/communities")
+      .then((resp) => resp.json())
+      .then(setCommunities)
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>Communities</h1>
+      {console.log(communities)}
     </div>
   );
 };

e361a7e15a53b6aa418bb27f30134bbda653b67a
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-11-29 18:37:57 -0800

    passes todoobjs to todo component

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import { useOutletContext } from 'react-router-dom';
 import Todo from "../components/todoCard";
 import Journal from "../components/journalCard";
+import {format, addDays} from 'date-fns';
 
 const Dashboard = () => {
   //const { user } = useOutletContext()
+  const [isDay, setIsDay] = useState(true);
+  const [todos, setTodos] = useState([]);
   const user = {
     id: 1,
     username: "Daniel Williamson",
@@ -13,16 +16,31 @@ const Dashboard = () => {
     interests: "writing",
     bio: "Explain vote agreement law moment."
   }
-  const todoObj = (userTodos) => {
-    const todo_by_day = {}
-    for (const todo of userTodos) {
-      console.log(todo.day)
-    }
 
-    return
+
+  const createTodoObj = (todos) => {
+    const today = new Date()
+    let todoDict = {}
+    //creates object where key is today + next 6 days
+    for (let i = 0; i < 7; i++) {
+      const currentDate = addDays(today, i)
+      todoDict = {...todoDict, [format(currentDate, "yyyy-MM-dd")]: []}
+    }
+    //add corresponding todos
+    for (const todo of todos) {
+      if (todoDict[todo.date]) {
+        todoDict[todo.date] = [...todoDict[todo.date], todo]
+      }
+    }
+    return todoDict
   }
-  const [isDay, setIsDay] = useState(true);
-  const [todos, setTodos] = useState(null);
+
+  // filter out only todos that match the user_id
+  const userTodos = todos.filter((todo) => (
+    todo.user_id === user.id
+  ))
+  //grab the dictionary
+  const todoObj = createTodoObj(userTodos)
 
 //fetch all todos for the user
   useEffect(() => {
@@ -45,10 +63,7 @@ const Dashboard = () => {
 //POST need to include the date.now() feature to push in the current day 1 - 7
 
 
-//filter out only todos that match the user_id
-const userTodos = todos.filter((todo) => (
-  todo.user_id == user.id
-))
+
 
 
   return (
@@ -56,21 +71,18 @@ const userTodos = todos.filter((todo) => (
       <h1>Dashboard</h1>
       {isDay ? (
         <>
-          {/* {console.log(userTodos)} */}
-          {/* {console.log(user.id)} */}
-          {todoObj(userTodos)}
-          <Todo />
+          <Todo todoList={Object.values(todoObj)[0]}/>
           <Journal />
         </>
       ) : (
         <>
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
+          <Todo todoList={Object.values(todoObj)[0]} />
+          <Todo todoList={Object.values(todoObj)[1]} />
+          <Todo todoList={Object.values(todoObj)[2]} />
+          <Todo todoList={Object.values(todoObj)[3]} />
+          <Todo todoList={Object.values(todoObj)[4]} />
+          <Todo todoList={Object.values(todoObj)[5]} />
+          <Todo todoList={Object.values(todoObj)[6]} />
         </>
       )}
     </div>

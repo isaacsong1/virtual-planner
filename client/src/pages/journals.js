f4e79b5572ffc20933285e2a9ecc8c8fad5cf142
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-11-30 20:40:03 -0800

    fetches journal entries

--- client/src/pages/journals.js
+++ client/src/pages/journals.js
@@ -3,6 +3,22 @@ import { useOutletContext } from 'react-router-dom';
 
 const Journals = () => {
   const { user } = useOutletContext();
+  const [entries, setEntries] = useState([])
+  console.log(entries)
+
+  useEffect(() => {
+    fetch("/journals")
+    .then(r => r.json())
+    .then((journals) => {
+      const filteredJournal = journals.find((journal) => journal.user_id === user.id)
+      setEntries(filteredJournal.entries)
+    })
+    .catch((e) => console.log(e))
+  },[])
+
+
+
+
   //fetch all journals, filter just for journal where user_id = id
 
   return (
